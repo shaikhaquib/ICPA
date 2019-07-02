3bf68e8a3a5fdce7a28a43c7628083655396babf
Shaikh Aquib <shaikhaquib119@gmail.com>
2019-07-02 22:12:46 +0530

    Initial commit

---
+++ app/src/main/java/com/s/icpa/Profile.java
@@ -0,0 +1,19 @@
+package com.s.icpa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void profile(View view) {
+        startActivity(new Intent(getApplicationContext(),Edit_Profile.class));
+    }
+}
