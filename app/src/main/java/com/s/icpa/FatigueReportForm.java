803b4102225b4606ec5cc417fda5372973cd029c
Shaikh Aquib <shaikhaquib119@gmail.com>
2019-07-03 23:14:44 +0530

    FatigueReportForm pending

---
+++ app/src/main/java/com/s/icpa/FatigueReportForm.java
@@ -0,0 +1,13 @@
+package com.s.icpa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FatigueReportForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fatigue_report_form);
+    }
+}
